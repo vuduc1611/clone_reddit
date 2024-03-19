8342d6617f86a0df9a6390a0e3436ff4a8ffabaf
DucDz <vuduc1611@gmail.com>
2024-03-19 11:56:59 +0700

    first commit of front end

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 style={{color: "red"}}>Heloooooooooooooooooooooooooooooooo</h1>
+        <h1>Heloooooooooooooooooooooooooooooooo</h1>
+        <h1>Heloooooooooooooooooooooooooooooooo</h1>
+        <h1>Heloooooooooooooooooooooooooooooooo</h1>
+    </div>
+  );
+}
+
+export default App;
